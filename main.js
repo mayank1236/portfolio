723f0be03db3b5af32eabe96fea3bb8adcddfc43
Mayank Gupta <mayankgupta14042000@gmail.com>
2022-09-24 02:46:05 +0530

    arrow to cta, nav background transition on scroll

--- main.js
+++ main.js
@@ -1,1 +1,18 @@
-console.log('hi');
+//EVENTS
+window.onscroll = () => {
+    
+    fixedNavBg();
+}
+
+//HTML ELEMENTS
+let nav = document.querySelector('.navbar');
+
+//FUNCTIONS
+function fixedNavBg() {
+    let pos = window.pageYOffset;
+    let threshold = 82;
+    let opacity = (pos >= threshold) ? 1 : pos/threshold;
+    let shadow = `rgba(50, 50, 93, ${opacity * 0.25}) 0px 6px 12px -2px, rgba(0, 0, 0,  ${opacity * 0.3}) 0px 3px 7px -3px`;
+    nav.style.boxShadow = shadow;
+    nav.style.backgroundColor = `rgba(255, 255, 255, ${opacity})`;
+}
