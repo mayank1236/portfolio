0a666f1f1a9730f8ffe533a2d803a0f0b72b1ea8
Mayank Gupta <mayankgupta14042000@gmail.com>
2022-09-29 15:50:44 +0530

    logo size change on window width change and repsonsive, changed css file structure

--- main.js
+++ main.js
@@ -1,6 +1,17 @@
+//global
+toggleMenu();
+
 //EVENTS
+window.onresize = () => {
+    fixedNavBg();
+
+    toggleMenu();
+}
+
 window.onload = () => {
     fixedNavBg();
+
+    toggleMenu();
 }
 
 window.onscroll = () => {
@@ -10,6 +21,7 @@ window.onscroll = () => {
 //HTML ELEMENTS
 let nav = document.querySelector('.navbar');
 let logo = document.querySelector('.navbar .logo');
+let navMenu = document.querySelector('.navbar .nav');
 
 //FUNCTIONS
 function fixedNavBg() {
@@ -17,9 +29,8 @@ function fixedNavBg() {
     let threshold = 82;
     // Increases on scroll till 82 scroll position (min: 0, max: 1)
     let opacity = (pos >= threshold) ? 1 : pos/threshold;
-    
     let shadow = `rgba(50, 50, 93, ${opacity * 0.25}) 0px 6px 12px -2px, rgba(0, 0, 0,  ${opacity * 0.3}) 0px 3px 7px -3px`;
-    
+
     styling(
         nav, 
         [
@@ -28,12 +39,29 @@ function fixedNavBg() {
         ]
     );
 
-    styling(logo, [['fontSize', `${ (10 * (1 - opacity)) + 30 }px`]]);
+    styling(logo, [['fontSize', `calc(${ (10 * (1 - opacity)) + 'px'} + ${getLogoFont(window.innerWidth)})`]]);
 }
 
 function styling(ele, props = []) {
-
     props.map(prop => {
         ele.style[prop[0]] = prop[1];
     });
+}
+
+function getLogoFont(width) {
+    let logoSize = 0;
+
+    if(width <= 992 && width > 576) {
+        logoSize = 25;
+    } else if(width <= 576) {
+        logoSize = 20;
+    } else {
+        logoSize = 30;
+    }
+
+    return (logoSize + 'px');
+}
+
+function toggleMenu() {
+
 }
